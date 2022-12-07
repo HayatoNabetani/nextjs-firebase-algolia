ac683a9504d02d351c142b44a1ad19e1233dcb92
HayatoNabetani <nabe_baseball1111@yahoo.co.jp>
2022-12-07 12:30:07 +0900

    検索窓を整える

--- pages/search.tsx
+++ pages/search.tsx
@@ -10,6 +10,7 @@ import {
     useInstantSearch,
 } from "react-instantsearch-hooks-web";
 import { Post } from "../types/post";
+import { BeakerIcon } from "@heroicons/react/24/solid";
 
 const searchClient = algoliasearch(
     "B1MTY8H7DW",
@@ -39,7 +40,20 @@ const Search = () => {
             <InstantSearch searchClient={searchClient} indexName="posts">
                 {/* Todo: debounceでの検索ができないので、一旦飛ばす */}
                 {/* <SearchBox queryHook={debounce(serach, 2000)} /> */}
-                <SearchBox queryHook={serach} />
+                <SearchBox
+                    classNames={{
+                        root: "relative inline-block",
+                        input: "rounded-full border-slate-300 pr-10",
+                        submitIcon: "hidden",
+                        resetIcon: "hidden",
+                    }}
+                    submitIconComponent={() => (
+                        <span className="absolute right-0 p-2 w-10 top-1/2 -translate-y-1/2">
+                            <BeakerIcon className="w-5 h-5 text-slate-500" />
+                        </span>
+                    )}
+                    queryHook={serach}
+                />
                 <NoResultsBoundary>
                     <Hits<Post> hitComponent={Hit} />
                 </NoResultsBoundary>
