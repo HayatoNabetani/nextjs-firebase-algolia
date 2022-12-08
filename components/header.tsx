f896a4416cb2c6583e9a8acd3cc9f46ff2d3070b
HayatoNabetani <nabe_baseball1111@yahoo.co.jp>
2022-12-08 10:27:24 +0900

    サイドバーを導入

--- components/header.tsx
+++ components/header.tsx
@@ -1,26 +1,48 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 import { useAuth } from "../context/auth";
+import Sidebar from "./sidebar";
 import UserMenu from "./user-menu";
+import { Bars3Icon } from "@heroicons/react/24/solid";
 
 const Header = () => {
     const { user, isLoading } = useAuth();
-
+    const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(false);
     if (isLoading) {
         return null;
     }
 
+    const closeModal = () => {
+        setIsSidebarOpen(false);
+    };
+
+    const openModal = () => {
+        setIsSidebarOpen(true);
+    };
+
     return (
-        <header className="py-4">
-            <div className="flex items-center h-16 border-b container">
-                <Link href="/">
-                    <Image src="/logo.svg" width={218} height={40} alt="logo" />
-                </Link>
-                <span className="flex-1"></span>
-                {/* ↑これおもろいな */}
-                {user ? <UserMenu /> : <Link href="/login">ログイン</Link>}
-            </div>
-        </header>
+        <>
+            <header className="py-4">
+                <div className="flex items-center h-16 border-b container">
+                    <button className="p-2 mr-1" onClick={openModal}>
+                        <Bars3Icon className="w-6 h-6" />
+                    </button>
+                    <Link href="/">
+                        <Image
+                            src="/logo.svg"
+                            width={218}
+                            height={40}
+                            alt="logo"
+                        />
+                    </Link>
+                    <span className="flex-1"></span>
+                    {/* ↑これおもろいな */}
+                    {user ? <UserMenu /> : <Link href="/login">ログイン</Link>}
+                </div>
+            </header>
+            <Sidebar isOpen={isSidebarOpen} closeModal={closeModal} />
+        </>
     );
 };
 
